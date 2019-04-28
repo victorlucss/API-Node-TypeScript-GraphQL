58eccac5baf4e431bfe4f38888b2982f432913f6
Victor Lucas <victorlucas@alu.ufc.br>
2019-04-28 19:20:18 -0300

    Importação de nova forma de conectar ao MongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,12 @@
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
 import config from './config/config';
+import * as mongo from './config/mongo';
 
 const port = process.env.port || 3000;
 
 dotenv.config();
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/test?retryWrites=true`, {useNewUrlParser: true})
-    .then(() => {
-        console.log("Conected to MongoDB");
-    }, (err: any) => {
-        console.error("Failed to connect to MongoDB", err);
-        
-    });
+mongo.connect(3000, 5);
 
 config.listen(port, () => {
     console.log(`Server listeninig on port ${port}`)
