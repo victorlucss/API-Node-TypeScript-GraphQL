7fc13f5c2523de4c680925edb96232fcb0a0454b
Victor Lucas <victorlucas@alu.ufc.br>
2019-04-28 19:19:45 -0300

    Extração de conexão com o Mongo para arquivo separado

---
+++ src/config/mongo.ts
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+let mongoIsConected = false;
+
+const connectToMongo = () => {
+    console.log("[MONGO] Conecting to MongoDB...");
+    mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/test?retryWrites=true`, {useNewUrlParser: true})
+        .then(() => {
+            mongoIsConected = true;
+            console.log("[MONGO] Conected to MongoDB");
+        }, (err: any) => {
+            mongoIsConected = false;
+            console.error("[MONGO] Failed to connect to MongoDB", err);
+        });
+};
+
+export function connect(interval: number, attemps: number){
+    setInterval(() => {
+        if(attemps === 0) {
+            console.error("[MONGO] Error in MongoDB connection!");
+            clearInterval();
+            interval
+        }else if(!mongoIsConected){
+            console.log("[MONGO] Retrying connection with MongoDB");
+            connectToMongo();
+        }
+
+        attemps--;
+    }, interval);
+
+    connectToMongo();
+
+};
