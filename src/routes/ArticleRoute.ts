a7d3a6d737e3a468c086c99e1480ba14abdb563a
Victor Lucas <victorlucas@alu.ufc.br>
2019-05-10 19:38:57 -0300

    Route of article

---
+++ src/routes/ArticleRoute.ts
@@ -0,0 +1,24 @@
+import { Application } from 'express';
+import RouteInterface from '../models/interface/RouteInterface';
+import ArticleController from '../controllers/ArticleController';
+
+export default class ArticleRoute implements RouteInterface {
+
+    private controller: ArticleController;
+
+    constructor(){
+        this.controller = new ArticleController();
+    }
+
+    public applyRoute(app: Application): void {
+
+        app.get('/article', this.controller.getArticles);
+
+        app.post('/article', this.controller.saveArticle);
+
+        // app.delete('/:id', this.controller.deleteArticle);
+
+        // app.put('/:id', this.controller.updateArticle);
+    }
+
+};
