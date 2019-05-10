ffca04b6bd31cedcba61164ee82b9771674ae401
Victor Lucas <victorlucas@alu.ufc.br>
2019-05-10 19:39:22 -0300

    ArticleService - here will be "all" logic

---
+++ src/services/ArticleService.ts
@@ -0,0 +1,42 @@
+import ArticleRepository from "../repositories/ArticleRepository";
+import ArticleInterface from "../models/interface/ArticleInterface";
+import ErrorException from "../base/ErrorException";
+import Response from "../base/Response";
+import Article from "../models/Article";
+
+export default class ArticleService {
+    private repository: ArticleRepository;
+
+    constructor(){
+        this.repository = new ArticleRepository();
+    };
+
+    public async getArticles(): Promise<Response> {
+        
+        let articles: ArticleInterface[] = await this.repository.find();
+
+        if(articles.length > 0) return new Response(articles);
+        
+        
+        throw new ErrorException("No content found.", 204);
+
+    }
+
+    public async saveArticle(article: ArticleInterface): Promise<Response> {
+
+        try {
+
+            let articleSave: ArticleInterface = new Article(article);
+            let validate = await articleSave.validateSync();
+    
+            if(validate)
+                throw new ErrorException(validate.message, 400);
+
+            this.repository.save(articleSave);            
+            return new Response("Article successfuly saved!");
+
+        } catch(ex){
+            throw ex;
+        }
+    }
+};
