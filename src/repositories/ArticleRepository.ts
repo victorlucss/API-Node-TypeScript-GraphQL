692163c58a65b0014572a7082d713917cb268a98
Victor Lucas <victorlucas@alu.ufc.br>
2019-05-14 08:25:44 -0300

    ArticleRepository implements RepositoryInterface and findById, edit and
    delete methods

--- src/repositories/ArticleRepository.ts
+++ src/repositories/ArticleRepository.ts
@@ -1,11 +1,20 @@
 import ArticleInterface from '../models/interface/ArticleInterface';
 import Article from  '../models/Article';
 import ErrorException from '../base/ErrorException';
+import RepositoryInterface from '../base/RepositoryInterface';
 
-export default class ArticleRepository {
+export default class ArticleRepository implements RepositoryInterface {
     
     constructor(){};
 
+    public async findById(id: number): Promise<ArticleInterface> {
+        let article: ArticleInterface = await Article.findById(id, (err) => {
+            if(err) throw new ErrorException(err.message, 404);
+        });       
+
+        return article;
+    };
+    
     public async find(): Promise<ArticleInterface[]> {
         let articles: ArticleInterface[] = await Article.find();
         
@@ -15,6 +24,18 @@ export default class ArticleRepository {
     public async save(article: ArticleInterface): Promise<void> {
         await article.save().catch(err => {
             throw new ErrorException(err.message, 500);
+        });
+    }
+
+    public async edit(id: number, article: ArticleInterface): Promise<void> {
+        let findArticle = await Article.findByIdAndUpdate(id, article, (err) => {
+            if(err) throw new ErrorException(err.message, 500);
+        });
+    }
+    
+    public async delete(id: number): Promise<void> {
+        let article = await Article.findByIdAndDelete(id, (err) => {
+            if(err) throw new ErrorException(err.message, 500);
         });
     }
 };
