9f5f9ce04243842d92b329d6d92d3e6d2a1c5307
Victor Lucas <victorlucas@alu.ufc.br>
2019-05-13 21:22:54 -0300

    Update on repository interface to support edit and delete

--- src/base/RepositoryInterface.ts
+++ src/base/RepositoryInterface.ts
@@ -3,5 +3,7 @@ export default interface DAOInterface {
 
     find(): Promise<object[]>;
     save(obj: object): Promise<void>;
+    edit(id: number, obj: object): Promise<void>;
+    delete(id: number): Promise<void>;
 
 };
